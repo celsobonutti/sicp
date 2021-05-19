494f3abebd6a0835cecca66e9def09490a49425c
Celso Bonutti <i.am@cel.so>
2021-05-19 19:57:47 -0300

    adds chapter 2.1.2

---
+++ chapter-2/rational.rkt
@@ -0,0 +1,44 @@
+#lang racket
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (define (make-pair x y)
+      (cons (/ x g) (/ y g)))
+    (if (equal? (negative? n) (negative? d))
+        (make-pair (abs n) (abs d))
+        (if (negative? n)
+            (make-pair n d)
+            (make-pair (- n) (- d)))
+        )))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (numer y) (denom x))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
---
+++ chapter-2/segment.rkt
@@ -0,0 +1,81 @@
+#lang racket
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (midpoint-segment segment)
+  (let* ([start (start-segment segment)]
+        [end (end-segment segment)]
+        [mid-x (average (x-point start) (x-point end))]
+        [mid-y (average (y-point start) (y-point end))])
+    (make-point mid-x mid-y)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+;; (define (make-rectangle left-top-corner right-bottom-corner)
+;;   (cons left-top-corner right-bottom-corner))
+
+;; (define (rec-top rectangle)
+;;   (cdr (car rectangle)))
+
+;; (define (rec-bottom rectangle)
+;;   (cdr (cdr rectangle)))
+
+;; (define (rec-left rectangle)
+;;   (car (car rectangle)))
+
+;; (define (rec-right rectangle)
+;;   (car (cdr rectangle)))
+
+(define (make-rectangle left top right bottom)
+  (list left top right bottom))
+
+(define (rec-top rectangle)
+  (cadr rectangle))
+
+(define (rec-left rectangle)
+  (car rectangle))
+
+(define (rec-right rectangle)
+  (caddr rectangle))
+
+(define (rec-bottom rectangle)
+  (cadddr rectangle))
+
+(define (rec-height rectangle)
+  (abs (- (rec-top rectangle) (rec-bottom rectangle))))
+
+(define (rec-width rectangle)
+  (abs (- (rec-left rectangle) (rec-right rectangle))))
+
+(define (rec-perimeter rectangle)
+  (+ (* 2 (rec-width rectangle))
+     (* 2 (rec-height rectangle))))
+
+(define (rec-area rectangle)
+  (* (rec-width rectangle)
+     (rec-height rectangle)))
